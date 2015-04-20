527fabb81ff6383f8ece46848feb2e329c131201
David Rovani <david@rovani.net>
2015-04-20 17:29:07 -0500

    Initial projects for the Core data models, data modeling objects, data services, and data testing. Added public assembly signing, code contracts, application versioning, and copyright notifications.

---
+++ Vigil.Data/Vigil.Data.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vigil.Data.Services")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vigil Projects")]
+[assembly: AssemblyProduct("Vigil.Data.Services")]
+[assembly: AssemblyCopyright("Copyright © Vigil Projects 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40c6e789-9d40-4385-8849-dc7cc793eff7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
