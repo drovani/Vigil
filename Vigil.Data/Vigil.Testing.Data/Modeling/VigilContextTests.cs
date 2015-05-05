df9e61081f22471546dd1fd1132c8ecfecc3a26c
David Rovani <david@rovani.net>
2015-05-05 16:48:48 -0500

    Decided I didn't like having fields for the context, testuser, and now. Put them back as variables in each test method, and added a ContractVerification(false) attribute to the whole class. I think I'm going to find myself adding that attribute to a lot of testing classes.

--- Vigil.Data/Vigil.Testing.Data/Modeling/VigilContextTests.cs
+++ Vigil.Data/Vigil.Testing.Data/Modeling/VigilContextTests.cs
@@ -14,92 +14,104 @@ namespace Vigil.Testing.Data.Modeling
     [ContractVerification(false)]
     public class VigilContextTests
     {
-        private VigilContext context;
-        private VigilUser testUser;
-        private DateTime now;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            testUser = new VigilUser { UserName = "TestUser" };
-            now = new DateTime(2015, 4, 23, 13, 33, 12, DateTimeKind.Utc);
-            context = new VigilContext(testUser, now);
-        }
-
-        [TestCleanup]
-        public void TestCleanup()
-        {
-            if (context != null)
-            {
-                context.Dispose();
-            }
-        }
-
         [TestMethod]
         public void Initialize_And_Create_Database_Cause_Database_To_Exists()
         {
-            Database db = context.Database;
-            db.Initialize(true);
-            if (db.Exists())
+            using (VigilContext context = new VigilContext())
             {
-                db.Delete();
-            }
-            db.Create();
+                context.Database.Initialize(true);
+                if (context.Database.Exists())
+                {
+                    context.Database.Delete();
+                }
+                context.Database.Create();
 
-            Assert.IsTrue(db.Exists());
+                Assert.IsTrue(context.Database.Exists());
+            }
         }
 
         [TestMethod]
         public void Explicit_Constructor_Sets_AffectedBy_And_Now()
         {
-            Assert.AreEqual(testUser, context.AffectedBy);
-            Assert.AreEqual(now, context.Now);
+            VigilUser testUser = new VigilUser { UserName = "TestUser" };
+
+            DateTime now = new DateTime(2015, 4, 23, 13, 33, 12, DateTimeKind.Utc);
+            using (VigilContext context = new VigilContext(testUser, now))
+            {
+                Assert.AreEqual(testUser, context.AffectedBy);
+                Assert.AreEqual(now, context.Now);
+            }
         }
 
         [TestMethod]
         public void Validate_VigilUser_TableName_Is_Correct()
         {
-            string tableName = GetTableName<VigilUser>();
+            string tableName;
+            using (VigilContext context = new VigilContext())
+            {
+                tableName = GetTableName<VigilUser>(context);
+            }
             Assert.AreEqual("[vigil].[VigilUser]", tableName);
         }
 
         [TestMethod]
         public void Validate_VigilUserClaim_TableName_Is_Correct()
         {
-            string tableName = GetTableName<VigilUserClaim>();
+            string tableName;
+            using (VigilContext context = new VigilContext())
+            {
+                tableName = GetTableName<VigilUserClaim>(context);
+            }
             Assert.AreEqual("[vigil].[VigilUserClaim]", tableName);
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login"), TestMethod]
         public void Validate_VigilUserLogin_TableName_Is_Correct()
         {
-            string tableName = GetTableName<VigilUserLogin>();
+            string tableName;
+            using (VigilContext context = new VigilContext())
+            {
+                tableName = GetTableName<VigilUserLogin>(context);
+            }
             Assert.AreEqual("[vigil].[VigilUserLogin]", tableName);
         }
 
         [TestMethod]
         public void Validate_VigilUserRole_TableName_Is_Correct()
         {
-            string tableName = GetTableName<VigilUserRole>();
+            string tableName;
+            using (VigilContext context = new VigilContext())
+            {
+                tableName = GetTableName<VigilUserRole>(context);
+            }
             Assert.AreEqual("[vigil].[VigilUserRole]", tableName);
         }
 
         [TestMethod]
         public void Validate_VigilRole_TableName_Is_Correct()
         {
-            string tableName = GetTableName<VigilRole>();
+            string tableName;
+            using (VigilContext context = new VigilContext())
+            {
+                tableName = GetTableName<VigilRole>(context);
+            }
             Assert.AreEqual("[vigil].[VigilRole]", tableName);
         }
 
         [TestMethod]
         public void Set_Method_Returns_Valid_Objects()
         {
-            Assert.IsInstanceOfType(context.Set<VigilUser>(), typeof(IDbSet<VigilUser>));
+            VigilUser testUser = new VigilUser { UserName = "TestUser" };
+            DateTime now = new DateTime(2015, 04, 29, 11, 51, 14, DateTimeKind.Utc);
+            using (VigilContext context = new VigilContext(testUser, now))
+            {
+                Assert.IsInstanceOfType(context.Set<VigilUser>(), typeof(IDbSet<VigilUser>));
+            }
         }
 
-        private string GetTableName<TEntity>() where TEntity : class
-        {
-            Contract.Requires<ArgumentNullException>(context != null);
 
+        private static string GetTableName<TEntity>(DbContext context) where TEntity : class
+        {
             ObjectContext objContext = ((IObjectContextAdapter)context).ObjectContext;
             var sql = objContext.CreateObjectSet<TEntity>().ToTraceString();
             Regex regex = new Regex("FROM (?<table>.*) AS");
