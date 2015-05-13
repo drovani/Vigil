39136bf7d04cf27f765c82e980f279649e0c5112
David Rovani <david@rovani.net>
2015-05-13 13:12:55 -0500

    Removed Contract assumptions from test class, and disabled contract verification at the class level. Unit tests should cover checking to make sure that all contracts are in place.

--- Vigil.Data/Vigil.Testing.Data/TestClasses/TestTypeBase.cs
+++ Vigil.Data/Vigil.Testing.Data/TestClasses/TestTypeBase.cs
@@ -4,18 +4,16 @@ using Vigil.Data.Core;
 
 namespace Vigil.Testing.Data.TestClasses
 {
+    [ContractVerification(false)]
     internal class TestTypeBase : TypeBase
     {
         protected TestTypeBase(string typeName)
             : base(typeName)
         {
-            Contract.Assume(!String.IsNullOrEmpty(typeName));
         }
 
         internal static TestTypeBase CreateType(string typeName)
         {
-            Contract.Assume(!String.IsNullOrEmpty(typeName));
-
             return new TestTypeBase(typeName);
         }
     }
