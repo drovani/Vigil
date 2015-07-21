2166166a151c0e6bf3a0c030fcc50baae6e36524
David Rovani <david@rovani.net>
2015-07-21 17:01:29 -0500

    Added test fo the VigilRoleManager

---
+++ Vigil.Identity/Vigil.Testing.Identity/Model/VigilRoleManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vigil.Identity.Model;
+using Vigil.Testing.Identity.TestClasses;
+
+namespace Vigil.Testing.Identity.Model
+{
+    [TestClass()]
+    public class VigilRoleManagerTests
+    {
+        [TestMethod()]
+        public void VigilRoleManagerTest()
+        {
+            var rolestore = new InMemoryRoleStore();
+            VigilRoleManager vrman = new VigilRoleManager(rolestore);
+
+            Assert.IsNotNull(vrman);
+        }
+    }
+}
